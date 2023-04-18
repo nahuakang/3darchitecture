4eb21ec9215f551e8b5ad1dd16d00a9487a001ef
Nahua Kang <kangnahua@gmail.com>
2023-04-18 21:41:20 +0200

    Add player movement (simpler)

--- player.gd
+++ player.gd
@@ -3,7 +3,10 @@ extends CharacterBody3D
 
 @export var _mouse_sensitivity: float = 0.1
 @export var _move_speed: float = 5.0
+@export var _jump_velocity: float = 4.5
 
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var velocity_y = 0
 
 # Movement vectors
 var movement_vector: Vector3
@@ -16,7 +19,7 @@ func _ready() -> void:
 
 
 func _physics_process(delta) -> void:
-	move()
+	move(delta)
 
 
 func _input(event: InputEvent) -> void:
@@ -36,18 +39,32 @@ func aim(event: InputEvent) -> void:
 		$Camera3D.rotation_degrees.x = clamp(current_tilt, -90, 90)
 
 
-func move() -> void:
-	if (Input.is_action_pressed("move_forward")):
-		forward_movement = -transform.basis.z
-	elif (Input.is_action_pressed("move_backward")):
-		forward_movement = transform.basis.z
+func move(delta) -> void:
+#	if (Input.is_action_pressed("move_forward")):
+#		forward_movement = -transform.basis.z
+#	elif (Input.is_action_pressed("move_backward")):
+#		forward_movement = transform.basis.z
+#
+#	if (Input.is_action_pressed("move_left")):
+#		sideways_movement = -transform.basis.x
+#	elif (Input.is_action_pressed("move_right")):
+#		sideways_movement = transform.basis.x
+#
+#	movement_vector = (forward_movement + sideways_movement).normalized()
+#	velocity = movement_vector * _move_speed
 
-	if (Input.is_action_pressed("move_left")):
-		sideways_movement = -transform.basis.x
-	elif (Input.is_action_pressed("move_right")):
-		sideways_movement = transform.basis.x
+	var horizontal_velocity = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 
-	movement_vector = (forward_movement + sideways_movement).normalized()
-	velocity = movement_vector * _move_speed
+	velocity = (horizontal_velocity.x * global_transform.basis.x + horizontal_velocity.y * global_transform.basis.z) * _move_speed
+
+	if is_on_floor():
+		velocity_y = 0
+
+		if Input.is_action_just_pressed("jump"):
+			velocity_y = _jump_velocity
+	else:
+		velocity_y -= gravity * delta
+
+	velocity.y = velocity_y
 
 	move_and_slide()
