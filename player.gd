72e286fe48e6edab0c890660c6d18064bf078030
Nahua Kang <kangnahua@gmail.com>
2023-04-18 20:51:27 +0200

    Add player movement (without acceleration, etc.)

--- player.gd
+++ player.gd
@@ -2,10 +2,21 @@ extends CharacterBody3D
 
 
 @export var _mouse_sensitivity: float = 0.1
+@export var _move_speed: float = 5.0
+
+
+# Movement vectors
+var movement_vector: Vector3
+var forward_movement: Vector3
+var sideways_movement: Vector3
 
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+func _physics_process(delta) -> void:
+	move()
 
 
 func _input(event: InputEvent) -> void:
@@ -23,3 +34,20 @@ func aim(event: InputEvent) -> void:
 		var current_tilt: float = $Camera3D.rotation_degrees.x
 		current_tilt -= mouse_motion.relative.y * _mouse_sensitivity
 		$Camera3D.rotation_degrees.x = clamp(current_tilt, -90, 90)
+
+
+func move() -> void:
+	if (Input.is_action_pressed("move_forward")):
+		forward_movement = -transform.basis.z
+	elif (Input.is_action_pressed("move_backward")):
+		forward_movement = transform.basis.z
+
+	if (Input.is_action_pressed("move_left")):
+		sideways_movement = -transform.basis.x
+	elif (Input.is_action_pressed("move_right")):
+		sideways_movement = transform.basis.x
+
+	movement_vector = (forward_movement + sideways_movement).normalized()
+	velocity = movement_vector * _move_speed
+
+	move_and_slide()
